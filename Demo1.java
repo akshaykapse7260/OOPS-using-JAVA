fb9c6f11c5e235b233ad5c411fc4e99c26bf0f19
Akshay Kapse <akshaykapse11@gmail.com>
2023-06-17 14:50:53 +0530

    m1

---
+++ Demo1.java
@@ -0,0 +1,57 @@
+// class A {
+//     public void show(){
+//             for(int i=1;i<=10;i++){
+//             System.out.println("Hi");
+//         }
+//     }
+// }
+// class B {
+//     public void show(){
+//         for(int i=1;i<=10;i++){
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
+// public class Demo1 {
+//     public static void main(String[] args) {
+//         A obj1 = new A();
+//         B obj2 = new B();
+
+//         obj1.show();
+//         obj2.show();
+//     }
+    
+// }
+// class A{
+// 	public void show() {
+// 		for(int i=1;i<=10;i++) {
+// 			System.out.println("Hi");
+// 		}
+// 	}
+	
+// }
+// class B{
+
+// 	public void show() {
+// 		for(int i=1;i<=10;i++) {
+// 			System.out.println("Hello");
+// 		}
+// 	}	
+// }
+class A{
+    public void show(){
+        System.out.println(Hello);
+    }
+}
+class B{
+    public void show(){
+        System.out.println("Hi");
+    }
+}
+public class Demo {
+	public static void main(String[] args) {
+		
+	}
+
+}
